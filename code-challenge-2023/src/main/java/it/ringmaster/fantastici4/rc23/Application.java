83e1f24276c37df9ca89f8f9b79b1321c94906b6
lt-ringm <l.trombetti@ringmaster.it>
2023-03-09 16:08:27 +0100

    Added basic code.

--- code-challenge-2023/src/main/java/it/ringmaster/fantastici4/rc23/Application.java
+++ code-challenge-2023/src/main/java/it/ringmaster/fantastici4/rc23/Application.java
@@ -1,8 +1,38 @@
 package it.ringmaster.fantastici4.rc23;
 
+import it.ringmaster.fantastici4.rc23.common.F4Reader;
+import it.ringmaster.fantastici4.rc23.common.F4Writer;
+
 public class Application {
+
+    private static final String inputFile = "C:\\Users\\utente\\workspace\\code-challenge\\ReplyCodeChallenge2019\\src\\main\\resources\\input\\fourth_adventure.in";
+    private static final String outputFile = "C:\\Users\\utente\\workspace\\code-challenge\\ReplyCodeChallenge2019\\src\\main\\resources\\output\\output4.txt";
+
+    public static void getInput(){
+
+        F4Reader reader = new F4Reader("C:\\Users\\utente\\workspace\\code-challenge\\ReplyCodeChallenge2019\\src\\main\\resources\\input\\fourth_adventure.in");
+        reader.open();
+
+        // Spacchettare i dati in ingresso nelle varie classi
+
+        reader.close();
+    }
+
+    public static void printOutput(){
+        F4Writer writer = new F4Writer(outputFile);
+        writer.open();
+
+        // Mettere i dati su una riga da far printare nel file
+
+        writer.close();
+    }
 
     public static void main(String[] args) {
 
+        getInput();
+
+        // Logica quì!
+
+        printOutput();
     }
 }
